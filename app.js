9a36561bc776e999954ba25785ae0a043b15ca90
Nitinkumar Aswar <nitinkumaraswar94@gmail.com>
2025-04-23 11:23:56 +0530

    Update app.js
    sample code created

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+app.use('/auth', require('./routes/auth'));
+app.use('/tasks', require('./routes/tasks'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
 
