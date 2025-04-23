b3e0560ecd94e410f2b2d9a51e6272e84b129cc3
Nitinkumar Aswar <nitinkumaraswar94@gmail.com>
2025-04-23 11:36:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
-app.use('/auth', require('./routes/auth'));
-app.use('/tasks', require('./routes/tasks'));
+// Route imports
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
 
+// Route usage
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
+
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
-
