44cfd44df243806250ba46ce7f866c696c9717af
Nitinkumar Aswar <nitinkumaraswar94@gmail.com>
2025-04-23 11:32:53 +0530

    Update auth.js
    created

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send('User registered');
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).send('Invalid credentials');
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  res.json({ token });
+});
+
+module.exports = router;
 
